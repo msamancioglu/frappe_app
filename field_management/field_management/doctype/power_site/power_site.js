959e85cfd72e97583d29327eb0ba63fed4343369
frappe <frappe@ubuntu.vm>
2021-12-28 16:21:28 +0000

    field management app first commit

---
+++ field_management/field_management/doctype/power_site/power_site.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, CAT and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Power Site', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ field_management/field_management/doctype/power_site/test_power_site.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, CAT and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestPowerSite(unittest.TestCase):
+	pass
